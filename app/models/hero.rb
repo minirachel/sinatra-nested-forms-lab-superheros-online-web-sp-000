dfbb930c58d45924e0b05e7089fbbf552bf4774e
minirachel <rachel.a.levinson@gmail.com>
2021-06-01 10:35:06 -0400

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,17 @@
+class Hero
+    attr_reader :name, :power, :biography
+
+    @@all = []
+
+    def initialize(args)
+        @name = args[:name]
+        @power = args[:power]
+        @biography = args[:biography]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
